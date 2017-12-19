c884214cdf492b690fd4de54442d094288c8e300
ksylla <karl-heinz.sylla@iais.fraunhofer.de>
2017-12-19 15:05:44 +0100

    some logs to explore init- daemon

--- sc4-zookeeper/zookeeper-startup.sh
+++ sc4-zookeeper/zookeeper-startup.sh
@@ -3,6 +3,8 @@
 set -e
 
 echo `date` $0
+echo "XXX zookeeper INIT_DAEMON_BASE_URI = '$INIT_DAEMON_BASE_URI'   DELAY = '$DELAY'"
+echo "XXX zookeeper ENABLE_INIT_DAEMON = '$ENABLE_INIT_DAEMON'  INIT_DAEMON_STEP = '$INIT_DAEMON_STEP'"
 
 /wait-for-step.sh
 /execute-step.sh
@@ -10,7 +12,7 @@ echo `date` $0
 if [[ $ENABLE_INIT_DAEMON = "true" ]] ;then
     (
         while ! ( /healthcheck ) ;do echo expect to become healthy; sleep 1; done
-        echo XXX $0 initialisation finished, service is healthy
+        echo $0 initialisation finished, service is healthy
         /finish-step.sh
     ) &
 fi
